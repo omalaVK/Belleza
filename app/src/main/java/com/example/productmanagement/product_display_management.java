45cf5e53df48b788dc2f3395510af0564b3d7d68
asus <omalaVK>
2021-08-14 23:45:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/productmanagement/product_display_management.java
@@ -0,0 +1,14 @@
+package com.example.productmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_display_management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_display_management);
+    }
+}
